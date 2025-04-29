a73a12e011a41a296ecbf367a01ebe6663f7ca2b
Cynthia Gallegos Rodriguez <gallegck@rose-hulman.edu>
2025-04-28 22:36:14 -0400

    RunViewer window looks nice; timer implemented

--- Controller GUI/ControllerGUI_test.py
+++ Controller GUI/ControllerGUI_test.py
@@ -13,6 +13,10 @@ data_file = ""    #filepath to .csv file where data will be stored
 V_inst = 7.223
 I_inst = 302
 P_inst = 2181.34
+S_inst = "DRIVING"
+E_inst = 200
+countdown = 90.0
+timerDone = False
 
 # ===================================================================================================================
 class LogViewer(QWidget):
@@ -72,57 +76,110 @@ class RunViewer(QWidget):
         self.start_button = QPushButton("Start")
         self.stop_button = QPushButton("Stop")
         
-        # Set up the timer to call update_progress_bars every 100ms
-        timer = QTimer()
-        timer.timeout.connect(lambda: self.update_progress_bars(self))  # Connect the update function
-        timer.start(100)  # Call every 100ms
+        all_buttons = [self.start_button, self.stop_button]
         
-        # Voltage label and progress bar
-        self.voltage_label = QLabel("Voltage (V)" + str(V_inst), self)
-        self.current_label = QLabel("Current (mA)" + str(I_inst), self)
-        self.power_label = QLabel("Power (mW)" + str(P_inst), self)
+        for i in all_buttons:
+            i.setMinimumSize(80, 200)  # (ancho, largo)
         
-        self.voltage_progress = QProgressBar(self)
-        self.voltage_progress.setRange(0, 12)  # Progress bar from 0 to 100%
-        self.voltage_progress.setTextVisible(True)  # Show percentage
+        # Set up the timer to call update_run_graphics every 200ms
+        timer = QTimer(self)
+        timer.timeout.connect(self.update_run_graphics)  # Connect the update function
+        timer.start(200)  # Call every 200ms
         
-        self.current_progress = QProgressBar(self)
-        self.current_progress.setRange(0, 2000)  # Progress bar from 0 to 100%
-        self.current_progress.setTextVisible(True)  # Show percentage
+        self.instructions_label = QLabel("Insert RunViewer Instructions Here.")
+        self.instructions_label.resize(50, 30)
         
-        self.power_progress = QProgressBar(self)
-        self.power_progress.setRange(0, 2000)  # Progress bar from 0 to 100%
-        self.power_progress.setTextVisible(True)  # Show percentage
+        # Labels
+        self.voltage_label = QLabel("Voltage: " + str(V_inst) + " V", self)
+        self.current_label = QLabel("Current: " + str(I_inst) + " mA", self)
+        self.power_label = QLabel("Power: " + str(P_inst) + " mW", self)
+        self.energy_label = QLabel("Energy: " + str(E_inst) + " J", self)
         
+        self.state_label = QLabel(S_inst)
+        self.time_label = QLabel("Time Remaining: " + str(countdown))
         
+        all_labels = [self.voltage_label, self.current_label, self.power_label, self.energy_label, self.state_label, self.time_label]
+        for i in all_labels:
+            i.setStyleSheet("QLabel { color: black; font-size: 20px; }")
+            i.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        # Progress Bars
+        self.voltage_progress = QProgressBar(self)      # VOLTAGE
+        self.current_progress = QProgressBar(self)      # CURRENT
+        self.power_progress = QProgressBar(self)        # POWER
+        self.energy_progress = QProgressBar(self)        # ENERGY
+        
+        p_bars = [self.voltage_progress, self.current_progress, self.power_progress, self.energy_progress]
+        for i in p_bars:
+            # Set the style sheet to change the chunk color
+            self.voltage_progress.setRange(0, 100)     # Progress bar from 0 to 100%
+            self.voltage_progress.setTextVisible(True) # Show percentage
+            i.setStyleSheet("""
+                    QProgressBar {
+                        border: 2px solid grey;
+                        border-radius: 5px;
+                        background-color: #F0F0F0;
+                        text-align: center; /* Light gray background */
+                    }
+                    QProgressBar::chunk {
+                        background-color: #00C853;  /* Bright green fill */
+                        margin: 1px;
+                        height: 20px;  /* optional: how wide each chunk is */
+                    }
+                """)
+            i.setMinimumWidth(50)
+            i.setMinimumHeight(200)
+            i.setOrientation(Qt.Orientation.Vertical)
+            i.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        # Buttons
         self.start_button.clicked.connect(send_START)
         self.stop_button.clicked.connect(send_STOP)
 
+        # Adding to Layout - grid format
         layout = QGridLayout()
-        label = QLabel("Insert RunViewer Instructions Here.")
-        layout.addWidget(label, 0, 0)
+        layout.addWidget(self.instructions_label, 0, 0, 1, 6)
+        layout.addWidget(self.time_label, 1, 4, 1, 2)
         
-        layout.addWidget(self.voltage_label)
-        layout.addWidget(self.current_label)
-        layout.addWidget(self.power_label)
+        layout.addWidget(self.voltage_label, 4, 0, 1, 1)
+        layout.addWidget(self.current_label, 4, 1, 1, 1)
+        layout.addWidget(self.power_label, 4, 2, 1, 1)
+        layout.addWidget(self.energy_label, 4, 3, 1, 1)
+        layout.addWidget(self.state_label, 1, 0, 1, 3)
         
-        layout.addWidget(self.voltage_progress)
-        layout.addWidget(self.current_progress)
-        layout.addWidget(self.power_progress)
+        layout.addWidget(self.voltage_progress, 2, 0, 2, 1)
+        layout.addWidget(self.current_progress, 2, 1, 2, 1)
+        layout.addWidget(self.power_progress, 2, 2, 2, 1)
+        layout.addWidget(self.energy_progress, 2, 3, 2, 1)
         
-        layout.addWidget(self.start_button)
-        layout.addWidget(self.stop_button)
+        layout.addWidget(self.start_button, 2, 4, 1, 2)
+        layout.addWidget(self.stop_button, 3, 4, 1, 2)
+        
+        
+        
         self.setLayout(layout)
         
-    def update_progress_bars(self):
+    # updates the RunViewer window graphics every 200 ms
+    def update_run_graphics(self):
     # Update the progress bars and labels based on the latest voltage and current
-        self.voltage_label.setText("Voltage (V)" + str(V_inst))
-        self.current_label.setText("Current (mA)" + str(I_inst))
-        self.power_label.setText("Power (mW)" + str(P_inst))
+        global countdown
+         
+        self.voltage_label.setText("Voltage: " + str(V_inst) + " V")
+        self.current_label.setText("Current: " + str(I_inst) + " mA")
+        self.power_label.setText("Power: " + str(P_inst) + " mW")
+        self.energy_label.setText("Energy: " + str(E_inst) + " J")
+        self.state_label.setText(S_inst)
+        self.time_label.setText("Time Remaining: " + "{:.2f}".format(countdown))
         
-        self.voltage_progress.setValue(int((V_inst / 15.0) * 100))  # Set voltage as percentage (max 15V)
-        self.current_progress.setValue(int((I_inst / 500) * 100))  # Set current as percentage (max 5A)
-        self.power_progress.setValue(int((P_inst / 5000) * 100))
+        self.voltage_progress.setValue(int((V_inst / 12) * 100))  # Set voltage as percentage (max 15V)
+        self.current_progress.setValue(int((I_inst / 1500) * 100))  # Set current as percentage (max 5A)
+        self.power_progress.setValue(int((P_inst / 3000) * 100))
+        self.energy_progress.setValue(int((E_inst / 500) * 100))
+        
+        countdown =  countdown - 0.2  #timer tick
+        
+        if countdown <= 0:
+            timerDone = True
 
 
 # ======================================================================================
@@ -140,6 +197,10 @@ class MainWindow(QWidget):
         # Connect buttons to functions
         self.begin_button.clicked.connect(self.begin_run)
         self.logviewer_window_button.clicked.connect(self.open_logviewer_window)
+        
+        buttons = [self.begin_button, self.logviewer_window_button] 
+        for i in buttons:
+            i.setMinimumSize(80, 200)
 
         layout = QVBoxLayout()
         # Add the buttons to the window
